93f6405fa3df3539718d7a1b4e24eb0c66dd0084
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-02-29 19:56:30 +0800

    Change header folder

--- front_end/src/app/components/mainHeader/components/navigation/navigation.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header-navigation',
-  standalone: true,
-  imports: [],
-  templateUrl: './navigation.component.html',
-})
-export class NavigationComponent {}
