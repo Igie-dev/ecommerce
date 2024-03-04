8271c2c2f5650d1d00376a6c5d7d4c99f92be7a5
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-03-04 22:01:35 +0800

    Make data to struct

---
+++ back_end/dto/root.go
@@ -0,0 +1,24 @@
+package dto
+
+import "github.com/laptop-shop.api/model"
+
+func ToCustomer(c *model.Customer) *model.Customer {
+	return &model.Customer{
+		ID:         c.ID,
+		CreatedAt:  c.CreatedAt,
+		UpdatedAt:  c.UpdatedAt,
+		Email:      c.Email,
+		FirstName:  c.FirstName,
+		LastName:   c.LastName,
+		CustomerId: c.CustomerId,
+		Contact:    c.Contact,
+	}
+}
+
+func ToCustomers(customers []*model.Customer) []*model.Customer {
+	res := make([]*model.Customer, len(customers))
+	for i, customer := range customers {
+		res[i] = ToCustomer(customer)
+	}
+	return res
+}
