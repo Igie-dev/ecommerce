1ab64c037517e3b9f0f2d4400b601b36f98c17a9
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-03-04 22:03:22 +0800

    Customer controller and routes

---
+++ back_end/controller/customerController.go
@@ -0,0 +1,177 @@
+package controller
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/laptop-shop.api/database"
+	"github.com/laptop-shop.api/dto"
+	"github.com/laptop-shop.api/model"
+	repo "github.com/laptop-shop.api/repository"
+	vldt "github.com/laptop-shop.api/validator"
+)
+
+// Create new customer
+func CreateCustomer(c *fiber.Ctx) error {
+	customer := &model.CreateCustomer{}
+
+	if err := c.BodyParser(customer); err != nil {
+		// Return status 400 and error message.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+	// Create a new validator for a Customer model.
+	validate := vldt.NewValidator()
+	if err := validate.Struct(customer); err != nil {
+		// Return, if some fields are not valid.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"msg":    "invalid input found",
+			"errors": vldt.ValidatorErrors(err),
+		})
+	}
+
+	customerRepo := repo.NewCustomerRepo(database.GetDB())
+	// check user already exists
+
+	exists, err := customerRepo.Exists(customer.Email)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+
+	if exists {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+			"msg": "Customer with this email  already exists",
+		})
+	}
+
+	//Generate password
+	customer.Password, _ = GeneratePasswordHash([]byte(customer.Password))
+	if err := customerRepo.Create(customer); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+
+	dbCustomer, err := customerRepo.Get(customer.CustomerId)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"customer": dto.ToCustomer(dbCustomer),
+	})
+
+}
+
+// Get one customer
+func GetCustomer(c *fiber.Ctx) error {
+	ID := c.Params("id")
+
+	customerId := uuid.MustParse(ID)
+	customerRepo := repo.NewCustomerRepo(database.GetDB())
+	customer, err := customerRepo.Get(customerId)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"msg": "customer were not found",
+		})
+	}
+	return c.JSON(fiber.Map{
+		"customer": dto.ToCustomer(customer),
+	})
+}
+
+// Get all customer
+func GetAllCustomers(c *fiber.Ctx) error {
+	pageNo, pageSize := GetPagination(c)
+	customerRepo := repo.NewCustomerRepo(database.GetDB())
+
+	customers, err := customerRepo.All(pageSize, uint(pageSize*(pageNo-1)))
+
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"msg": "customers were not found",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"page":      pageNo,
+		"page_size": pageSize,
+		"count":     len(customers),
+		"customer":  dto.ToCustomers(customers),
+	})
+}
+
+// Update a customer
+func UpdateCustomer(c *fiber.Ctx) error {
+	ID := c.Params("id")
+
+	customerId := uuid.MustParse(ID)
+
+	customerRepo := repo.NewCustomerRepo(database.GetDB())
+	_, err := customerRepo.Get(customerId)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"msg": "customer were not found",
+		})
+	}
+	customer := &model.UpdateCustomer{}
+	if err := c.BodyParser(customer); err != nil {
+		// Return status 400 and error message.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+	// Create a new validator for a User model.
+	validate := vldt.NewValidator()
+	if err := validate.Struct(customer); err != nil {
+		// Return, if some fields are not valid.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"msg":    "invalid input found",
+			"errors": vldt.ValidatorErrors(err),
+		})
+	}
+	if err := customerRepo.Update(customerId, customer); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+	dbCustomer, err := customerRepo.Get(customerId)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"user": dto.ToCustomer(dbCustomer),
+	})
+}
+
+// Delete a customer
+func DeleteCustomer(c *fiber.Ctx) error {
+	ID := c.Params("id")
+
+	customerId := uuid.MustParse(ID)
+
+	customerRepo := repo.NewCustomerRepo(database.GetDB())
+
+	_, err := customerRepo.Get(customerId)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"msg": "user were not found",
+		})
+	}
+
+	err = customerRepo.Delete(customerId)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"msg": err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{})
+}
--- back_end/routes/customerRoutes.go
+++ back_end/routes/customerRoutes.go
@@ -1,93 +1,18 @@
 package routes
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
-	"github.com/laptop-shop.api/model"
-	"github.com/laptop-shop.api/utils"
-	"gorm.io/gorm"
+	"github.com/laptop-shop.api/controller"
 )
 
-type CustomerRepository struct {
-	DB *gorm.DB
-}
-
-func (r *CustomerRepository) CustomerRoutes(app *fiber.App) {
+func CustomerRoutes(app *fiber.App) {
 
 	api := app.Group("/customer")
 
-	//Model
-	customerModel := &model.Customer{}
-
-	//Migration
-	err := r.MigrateCustomer(customerModel)
-
-	if err != nil {
-		fmt.Println("Error migrating customer model:", err)
-	}
-
-	//JSONBody validation
-	validateCustomerMiddleWare := func(ctx *fiber.Ctx) error {
-		return utils.ValidateJSONBody(customerModel, ctx)
-	}
-
 	//Routes
-	api.Get("/", r.GetAllCustomer)
-	api.Post("/", validateCustomerMiddleWare, r.CreateNewCustomer)
-
-}
-
-//*Controllers
-
-// Migrate
-func (r *CustomerRepository) MigrateCustomer(model interface{}) error {
-	err := r.DB.AutoMigrate(model)
-	if err != nil {
-		fmt.Println("Error migrating customer model:", err)
-		// Handle the error, e.g., log it or return an error response
-	}
-	return err
-}
-
-// Create
-func (r *CustomerRepository) CreateNewCustomer(ctx *fiber.Ctx) error {
-
-	u := uuid.New()
-	//Create user from data and add cutomer id by generating randum string
-	customer := model.Customer{
-		CustomerId: u,
-	}
-
-	err := r.DB.Create(&customer).Error
-
-	if err != nil {
-		ctx.Status(http.StatusBadRequest).JSON(&fiber.Map{"message": "Failed to create customer"})
-		return nil
-	}
-	return ctx.Status(http.StatusCreated).JSON(&fiber.Map{"message": "Customer created successfully"})
-}
-
-// Read
-func (r *CustomerRepository) GetAllCustomer(ctx *fiber.Ctx) error {
-
-	customers := &[]model.Customer{}
-
-	err := r.DB.Find(&customers).Error
-
-	if err != nil {
-		ctx.Status(http.StatusNotFound).JSON(&fiber.Map{"message": "No customer found"})
-		return nil
-	}
-
-	if len(*customers) <= 0 {
-		ctx.Status(http.StatusNotFound).JSON(&fiber.Map{"message": "No customer found"})
-		return nil
-	}
-
-	return ctx.Status(200).JSON(fiber.Map{
-		"data": customers,
-	})
+	api.Post("/", controller.CreateCustomer)
+	api.Get("/", controller.GetAllCustomers)
+	api.Get("/:id", controller.GetCustomer)
+	api.Patch("/:id", controller.UpdateCustomer)
+	api.Delete("/:id", controller.DeleteCustomer)
 }
