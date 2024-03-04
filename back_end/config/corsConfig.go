eaa67f630394a37b4eb27632f38e4fa74bcec955
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-03-04 22:04:37 +0800

    Move pacakges

---
+++ back_end/config/corsConfig.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func Allowed_origins() string {
+	var frontEndUrl = os.Getenv("FRONT_END_ENDPOINT")
+	var allowedOrigins = []string{fmt.Sprintf("%s", frontEndUrl)}
+	allowed := strings.Join(allowedOrigins, ",")
+	return allowed
+}
+
+var CorsConfig = cors.Config{
+	AllowOrigins:     Allowed_origins(),
+	AllowHeaders:     "Origin, Content-Type, Accept,Option,Authorization,authorization",
+	AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+	AllowCredentials: true,
+}
