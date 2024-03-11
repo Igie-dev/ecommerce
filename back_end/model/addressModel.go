e337342124cf20b4cb2d3c2113769898a38fa429
Igie <baldesansoigieboysanchez@gmail.com>
2024-03-11 13:03:37 +0800

    Add address model

--- back_end/model/addressModel.go
+++ back_end/model/addressModel.go
@@ -16,4 +16,17 @@ type (
 		Region      string    `json:"region"`
 		Province    string    `json:"province"`
 	}
+ 
+CreateAddress struct {
+     CustomerId uuid.UUID `json:"customer_id" validate:"required"`
+    City string `json:"city" validate:"required"`
+    PostalCode string `json:"postal_code" validate:"required"`
+    Country string `json:"country" validate:"required"`
+    Street string `json:"street" validate:"required"`
+    HouseNumber string `json:"house_no" validate:"required"`
+    Barangay string `json:"barangay" validate:"required"`
+    Region string `json:"region" validate:"required"`
+    Province string `json:"province validate:"required"`
+
+  }
 )
